7adecc35b3e8dcaf378c32c55977b08a8879edfc
aadityachapagain <chapagain.aaditya@gmail.com>
2023-03-31 17:13:48 +1100

    update user input on postgres_dbt

--- dbt/dbt_handler/postgres_dbt.sh
+++ dbt/dbt_handler/postgres_dbt.sh
@@ -1,4 +1,3 @@
-
 read -p "Please input a postgres username:" POSTGRES_USER_
 
 while [ ! -n "$POSTGRES_USER_" ]; 
@@ -20,18 +19,11 @@ while [ ! -n "$POSTGRES_DATABASE_" ];
         read -p "Please input a postgres database name:" POSTGRES_DATABASE_
     done
 
-read -p "Please input a OPENLINEAGE_COMPANY_ID:" OPENLINEAGE_COMPANY_ID_
+read -p "Please input a OPENLINEAGE_API_KEY:" OPENLINEAGE_API_KEY_
 
-while [ ! -n "$OPENLINEAGE_COMPANY_ID_" ]; 
+while [ ! -n "$OPENLINEAGE_API_KEY_" ]; 
     do
-        read -p "Please input a OPENLINEAGE_COMPANY_ID:" OPENLINEAGE_COMPANY_ID_
-    done
-
-read -p "Please input a OPENLINEAGE_TOKEN:" OPENLINEAGE_TOKEN_
-
-while [ ! -n "$OPENLINEAGE_TOKEN_" ]; 
-    do
-        read -p "Please input a OPENLINEAGE_TOKEN:" OPENLINEAGE_TOKEN_
+        read -p "Please input a OPENLINEAGE_API_KEY:" OPENLINEAGE_API_KEY_
     done
 
 read -p "Please input a OPENLINEAGE_URL:" OPENLINEAGE_URL_
@@ -51,8 +43,6 @@ while [ ! -n "$OPENLINEAGE_NAMESPACE_" ];
 export POSTGRES_USER=${POSTGRES_USER_}
 export POSTGRES_PASSWORD=${POSTGRES_PASSWORD_}
 export POSTGRES_DATABASE=${POSTGRES_DATABASE_}
-
-OPENLINEAGE_API_KEY_="$OPENLINEAGE_COMPANY_ID_|$OPENLINEAGE_TOKEN_"
 
 echo your input dbt openlineage url="$OPENLINEAGE_URL_" openlineage APIKey="$OPENLINEAGE_API_KEY_"
 
