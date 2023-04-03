67d906888fd17ee6cbc44b4df43b26344227d493
chaudharigunjan <gunjan@cloudshuttle.com.au>
2023-04-03 10:44:10 +1000

    fix flow

--- dbt/dbt_handler/postgres_dbt.sh
+++ dbt/dbt_handler/postgres_dbt.sh
@@ -1,3 +1,4 @@
+
 read -p "Please input a postgres username:" POSTGRES_USER_
 
 while [ ! -n "$POSTGRES_USER_" ]; 
@@ -19,37 +20,18 @@ while [ ! -n "$POSTGRES_DATABASE_" ];
         read -p "Please input a postgres database name:" POSTGRES_DATABASE_
     done
 
-read -p "Please input a OPENLINEAGE_API_KEY:" OPENLINEAGE_API_KEY_
-
-while [ ! -n "$OPENLINEAGE_API_KEY_" ]; 
-    do
-        read -p "Please input a OPENLINEAGE_API_KEY:" OPENLINEAGE_API_KEY_
-    done
-
-read -p "Please input a OPENLINEAGE_URL:" OPENLINEAGE_URL_
-
-while [ ! -n "$OPENLINEAGE_URL_" ]; 
-    do
-        read -p "Please input a OPENLINEAGE_URL:" OPENLINEAGE_URL_
-    done
-
-
-read -p "Please input a OPENLINEAGE_NAMESPACE:" OPENLINEAGE_NAMESPACE_
-
-while [ ! -n "$OPENLINEAGE_NAMESPACE_" ]; 
-    do
-        read -p "Please input a OPENLINEAGE_NAMESPACE:" OPENLINEAGE_NAMESPACE_
-    done
 export POSTGRES_USER=${POSTGRES_USER_}
 export POSTGRES_PASSWORD=${POSTGRES_PASSWORD_}
 export POSTGRES_DATABASE=${POSTGRES_DATABASE_}
 
-echo your input dbt openlineage url="$OPENLINEAGE_URL_" openlineage APIKey="$OPENLINEAGE_API_KEY_"
+OPENLINEAGE_API_KEY_="$OPENLINEAGE_API_KEY"
+
+echo your input dbt openlineage url="$OPEN_LINEAGE_ENDPOINT" openlineage APIKey="$OPENLINEAGE_API_KEY_"
 
 
 export OPENLINEAGE_API_KEY=$OPENLINEAGE_API_KEY_
-export OPENLINEAGE_URL=$OPENLINEAGE_URL_
-export OPENLINEAGE_NAMESPACE=$OPENLINEAGE_NAMESPACE_
+export OPENLINEAGE_URL=$OPEN_LINEAGE_ENDPOINT
+export OPENLINEAGE_NAMESPACE=$OPEN_LINEAGE_NAMESPACE
 
 # dbt run 
 
---
+++ dbt/dbt_handler/run_postgres.sh
@@ -0,0 +1,5 @@
+brew update
+brew install postgresql
+brew services start postgresql
+psql postgres
+
